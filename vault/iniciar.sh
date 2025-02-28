1fd8fda231aca784c94e690f0d281a0a20ce414d
Rafael Perazzo <rafaelperazzo@gmail.com>
2025-02-28 14:32:56 -0300

    fix: criação de policy do vault corrigida

--- vault/iniciar.sh
+++ vault/iniciar.sh
@@ -11,11 +11,12 @@ docker compose up -d
 docker compose exec -T vault sh -c "vault operator init > /share/init.file"
 #vault operator init > /share/init.file
 export VAULT_ADDR=http://127.0.0.1:8200
+echo "export VAULT_ADDR=http://127.0.0.1:8200" >> ~/.bashrc 
 cat share/init.file
-read -p "Digite a primeira chave de unseal" seal1
-read -p "Digite a segunda chave de unseal" seal2
-read -p "Digite a terceira chave de unseal" seal3
-read -p "Digite o token root" token
+read -p "Digite a primeira chave de unseal: " seal1
+read -p "Digite a segunda chave de unseal: " seal2
+read -p "Digite a terceira chave de unseal: " seal3
+read -p "Digite o token root: " token
 vault operator unseal $seal1
 vault operator unseal $seal2
 vault operator unseal $seal3
@@ -23,9 +24,9 @@ vault login $token
 vault secrets enable -path /cppgi -version=2 kv
 vault kv put /cppgi/1 data=$(openssl rand -hex 32)
 vault kv put /cppgi/2 data=$(openssl rand -hex 32)
-vault policy write ./policy/cppgi_read.hcl
+vault policy write cppgi_read ./policy/cppgi_read.hcl
 vault token create -policy=cppgi_read > share/token_ro
 cat share/token_ro
-read -p "Digite o token somente leitura" token
+read -p "Digite o token somente leitura: " token
 vault login $token
 
